43e39ce3ad4d3934ee3c36751aa26ebdfe6d464e
glouglou000 <61392905+glouglou000@users.noreply.github.com>
2020-02-24 16:36:09 +0100

    Add files via upload

--- register/models.py
+++ register/models.py
@@ -1,3 +1,13 @@
+"""This is model from accounts app"""
+
 from django.db import models
 
-# Create your models here.
+class Accounts(models.Model):
+    """foodAccount model"""
+    
+    name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+
+
+    
--- register/views.py
+++ register/views.py
@@ -4,3 +4,65 @@ from django.shortcuts import render
 
 def compte(request):
     return render(request, "compte.html")
+
+
+def login_view(request):
+    """Here we define the login view"""
+
+    next = request.GET.get('next')
+    form = UserLoginForm(request.POST or None)
+    
+    if form.is_valid():
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(username=username,
+                            password=password)
+        
+        login(request, user)
+        
+        if next:
+            return redirect(next)
+        
+        return redirect('/')
+
+    context = {'form':form}
+
+    return render(request, 'login.html', context)
+
+
+def register_view(request):
+    """Here we define the register view"""
+
+    next = request.GET.get('next')
+    form = UserRegisterForm(request.POST or None)
+
+    if form.is_valid():
+
+        user = form.save(commit=False)
+        password = form.cleaned_data.get('password')
+        user.set_password(password)
+        user.save()
+        
+        acc = Accounts(name=user.username)
+        acc.save()
+        
+        new_user = authenticate(username=user.username,
+                                password=password)
+
+        login(request, new_user)
+
+        if next:
+            return redirect(next)
+        return redirect('/')
+
+    context = {'form':form}
+
+    return render(request, 'signup.html', context)
+
+
+def logout_view(request):
+    """Here we define logout session"""
+
+    logout(request)
+    print("déconnexion")
+    return redirect('/')
