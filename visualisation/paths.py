20a85bd8ffaaaf8a84160f8dafdcff5c4ebed11d
glouglou000 <61392905+glouglou000@users.noreply.github.com>
2020-02-25 23:34:00 +0100

    Add files via upload

---
+++ visualisation/paths.py
@@ -0,0 +1,2 @@
+upload_folder = r"C:\Users\jeanbaptiste\Desktop\animateyourface\animateyoureface\media\video_upload"
+upload_video = "/media/video_upload/{}/{}"
--- visualisation/views.py
+++ visualisation/views.py
@@ -1,22 +1,53 @@
 import os
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+path = os. getcwd()
+path_current = os.path.basename(path)
+
 
 from django.shortcuts import render
-from .paths import upload_folder, upload_video
+from .paths import *
 
-#from .app.paths import 
 
-# Create your views here.
+
+from .app.paths import dlib_model, eyes_image, blink_image
+
+
+
+from .app.pre_treatment.pre_test import search_video_size
+from .app.pre_treatment.writter import video_writter
+
+from .app.recuperate_points.face_points import load_model_dlib
+from .app.video_capture_to_face_APP_2 import video_capture_to_face
+
+
 def visualisation(request):
 
+    if request.method == 'POST':
 
-    username = request.user
-    path_user = upload_folder + "/" + str(username)
-    user_list = os.listdir(path_user)
+        username = request.user
+        video = request.POST.get('video')
 
-    
-    liste_path = [upload_video.format(username, i) for i in user_list]
+        video_path_media = BASE_DIR + "/" + video
 
-    context = {"video_user": liste_path}
+        predictor, detector = load_model_dlib(dlib_model)
+ 
+        video_size = search_video_size(video_path_media, predictor, detector, dlib_model)
+        print(video_size)
 
-    return render(request, "visualisation.html", context)
+        video_capture_to_face(video_path_media, video, eyes_image,
+                              blink_image, username, video_size)
+
+
+    else:
+
+        username = request.user
+        path_user = upload_folder + "/" + str(username)
+        user_list = os.listdir(path_user)
+
+        liste_path = [upload_video.format(username, i) for i in user_list]
+
+        context = {"video_user": liste_path}
+
+        return render(request, "visualisation.html", context)
